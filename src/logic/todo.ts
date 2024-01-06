9c159bd8f54ad23d57627de1a93643a9a64a29c7
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-05 22:31:54 -0500

    refactor - clear up logic in todo.ts

--- src/logic/todo.ts
+++ src/logic/todo.ts
@@ -28,32 +28,32 @@ class Todo {
     }
     
     setTitle(title: string) {
-        if (typeof title === "string") {
-            this.title = title
-        } else {
+        if (typeof title !== "string") {
             throw new TypeError("Expect type string, value not valid")
         }
+        this.title = title
     }
 
     getTitle(): string {
         return this.title
     }
 
-
     getId(): number {
         return this.id
     }
 
     setDueDate(dueDate: DueDate) {
-        if (!isNaN(dueDate.day) && !isNaN(dueDate.month) && !isNaN(dueDate.year)) {
-            if (dueDate.day > 1 && dueDate.day < 31 && dueDate.month > 1 && dueDate.month < 12 && dueDate.year > 2024 && dueDate.year < 9999 ) {
-                this.dueDate = dueDate
-            } else {
-                throw new RangeError("Day, month, and or year Value if out of range, Date invalid")
-            }
-        } else {
+        const isValidDay = dueDate.day > 1 && dueDate.day < 31 
+        const isValidMonth = dueDate.month > 1 && dueDate.month < 12 
+        const isValidYear = dueDate.year > 2024 && dueDate.year < 9999 
+
+        if (isNaN(dueDate.day) || isNaN(dueDate.month) || isNaN(dueDate.year)) {
             throw new TypeError("Exexpt dueDate objects keys day, month, and year value to be of type number")
         }
+        if (!isValidDay || !isValidMonth || !isValidYear) {
+            throw new RangeError("Day, month, and or year Value if out of range, Date invalid")
+        }
+        this.dueDate = dueDate
     }
 
     getDueDate(): DueDate {
@@ -62,11 +62,10 @@ class Todo {
 
 
     setNote(note: string) {
-       if (typeof note === "string") {
-           this.note = note
-       } else {
+       if (typeof note !== "string") {
            throw new TypeError("Expect value of note to be a string, provided type invalid")
        }
+       this.note = note
     }
 
     getNote(): string | undefined {
@@ -74,12 +73,13 @@ class Todo {
     }
 
     setPriority(priority: Priority) {
-        if (priority > 0 && priority < 3) throw new TypeError("Invalid value of priority must be positive number less than 4")
+        if (priority < 0 || priority > 3) {
+            throw new TypeError("Invalid value of priority must be positive number less than 4")
+        }
         this.priority = priority
     } 
 
     getPriority(): Priority {
         return this.priority
     }
-
 }
