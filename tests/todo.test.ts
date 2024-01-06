f9b14032cfe6c5e7808d610b558e2418e6154213
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-05 23:16:14 -0500

    feat - make test case more clear - set up babel to make test work on ts

--- tests/todo.test.ts
+++ tests/todo.test.ts
@@ -17,35 +17,35 @@ describe('Todo class', () => {
 })
 
 describe('Todo class', () => {
-    test('setTitle method on can only be string', () => {
+    test('setTitle method on cannot be set to a number', () => {
         const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.note)
         expect(() => todo.setTitle(1)).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
-    test('setTitle method on can only be string', () => {
+    test('setTitle method on cannot be set to a boolean', () => {
         const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.note)
         expect(() => todo.setTitle(true)).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
-    test('setTitle method on can only be string', () => {
+    test('setTitle method on cannot be set to a string of empty charaters ', () => {
         const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.note)
         expect(() => todo.setTitle(" ")).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
-    test('setTitle method on can only be string', () => {
+    test('setTitle method on cannot be set to null', () => {
         const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.note)
         expect(() => todo.setTitle(null)).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
-    test('setTitle method on can only be string', () => {
+    test('setTitle method on cannot be set to undefined', () => {
         const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.note)
         expect(() => todo.setTitle(undefined)).toThrow("Expect type string, value not valid")
     })
