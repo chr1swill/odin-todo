dc843e3feef5bf7395afca4f237db64667f19a2f
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-06 16:00:08 -0500

    fix - test to work for Todo class logic

--- tests/todo.test.ts
+++ tests/todo.test.ts
@@ -2,7 +2,6 @@ import { Todo } from '../src/logic/todo'
 
 const testData = {
     title: "Test Todo", 
-    id: 1, 
     dueDate: {day: 1, month: 1, year: 2024}, 
     priority: 0, 
     status: false,
@@ -12,24 +11,24 @@ const testData = {
 
 describe('Todo class', () => {
     test('getInstances should return the current number of instances', () => {
-        const todo1 = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
-        const todo2 = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
-        const todo3 = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
-        const todo4 = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo1 = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo2 = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo3 = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo4 = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         expect(Todo.getIntances()).toBe(4)
     })
 })
 
 describe('Todo class', () => {
-    test('deleteInstances should deincreament number of instances', () => {
+    test('deleteInstances method should deincreament number of instances', () => {
         Todo.deletedInstance()
         expect(Todo.getIntances()).toBe(3)
     })
 })
 
 describe('Todo class', () => {
-    test('getTitle method should change title', () => {
-        const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+    test('setTitle method should change title', () => {
+        const todo = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         todo.setTitle('New Title')
         expect(todo.getTitle()).toBe('New Title')
     })
@@ -37,35 +36,37 @@ describe('Todo class', () => {
 
 describe('Todo class', () => {
     test('setTitle method on cannot be set to a number', () => {
-        const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         expect(() => todo.setTitle(1)).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
     test('setTitle method on cannot be set to a boolean', () => {
-        const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         expect(() => todo.setTitle(true)).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
     test('setTitle method on cannot be set to a string of empty charaters ', () => {
-        const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         expect(() => todo.setTitle(" ")).toThrow("Title cannot be an empty string")
     })
 })
 
 describe('Todo class', () => {
     test('setTitle method on cannot be set to null', () => {
-        const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         expect(() => todo.setTitle(null)).toThrow("Expect type string, value not valid")
     })
 })
 
 describe('Todo class', () => {
     test('setTitle method on cannot be set to undefined', () => {
-        const todo = new Todo(testData.title, testData.id, testData.dueDate, testData.priority, testData.status,testData.note)
+        const todo = new Todo(testData.title, testData.dueDate, testData.priority, testData.status,testData.note)
         expect(() => todo.setTitle(undefined)).toThrow("Expect type string, value not valid")
     })
 })
+
+
