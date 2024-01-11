956a6c4980d837c1ada8a52601e90e51754aaeea
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-10 20:40:15 -0500

    refactor - replace repeat logic with private function - add method to get and set complete on todo

--- src/logic/todo.js
+++ src/logic/todo.js
@@ -6,6 +6,13 @@ const Priority = {
     HIGH: 3
 }
 
+/** @enum { string } */
+const AllowedTypes = {
+    NUMBER: 'number',
+    STRING: 'string', 
+    BOOLEAN: 'boolean'
+}
+
 class Todo {
     /** @type { string }*/
     #id  
@@ -14,129 +21,102 @@ class Todo {
         this.#id = (Date.now() + Math.random()).toString()
         localStorage.setItem(this.#id, JSON.stringify(this)) 
     }
-    
+
     /** @returns { string } todo unique id number as a string */
     get id() {
         return this.#id
     }
 
-    /** @returns { string } todo title as a string */
-    get title() {
+    /**
+     * handle getting item form local storage on todo properties
+     *
+     * @param { string } property - key you would like to access the value to on this todo object 
+     * */
+    #handleLocalStorageGet(property) {
         const todoString = localStorage.getItem(this.#id)
         if (todoString === null) {
             throw new ReferenceError('Not able to access value of null')
         }
 
         const todoObj = JSON.parse(todoString)
-        if (todoObj.title !== undefined) {
-            return todoObj.title
+        if (todoObj[property] !== undefined) {
+            return todoObj[property]
         }
         throw new ReferenceError('Not able to access undefined property')
+    }
+
+    /**
+     * handle setting value of todo properies in local storage 
+     *
+     * @param { string } property - key of the value to you would like to modify 
+     * @param { number | string | boolean } newValue - new value you would like to set the key 
+     * @param { AllowedTypes } newValueType - type of the newValue, need to be boolean, number, or string
+     * */
+    #handleLocalStorageSet(property, newValue, newValueType) {
+        if (typeof newValue !== newValueType) {
+            throw new TypeError(`Value must be a ${newValueType}`)
+        }
+
+        const todoString = localStorage.getItem(this.#id)
+        if (todoString === null) {
+            throw new ReferenceError('Not able to access value of null')
+        }
+
+        const todoObj  = JSON.parse(todoString)
+        if (typeof property !== 'string') {
+            throw new TypeError('Value must be string')
+        }
+        todoObj[property] = newValue
+        localStorage.setItem(this.#id, JSON.stringify(todoObj))
+    }
+
+    /**@returns { string | Error }*/
+    get title() {
+        return this.#handleLocalStorageGet('title')
     }
 
     /**@param { string } title */
     set title(title) {
-        if (typeof title !== 'string') {
-            throw new TypeError('Value must be a string')
-        }
-
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
-
-        const todoObj  = JSON.parse(todoString)
-        todoObj.title = title 
-        localStorage.setItem(this.#id, JSON.stringify(todoObj))
+        this.#handleLocalStorageSet('title', title, AllowedTypes.STRING)
     }
 
-    /** @returns { string } todo note as a string */
+    /**@returns { string | Error }*/
     get note() {
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
-
-        const todoObj = JSON.parse(todoString)
-        if (todoObj.note !== undefined) {
-            return todoObj.note
-        }
-        throw new ReferenceError('Not able to access undefined property')
+        return this.#handleLocalStorageGet('note')
     }
 
     /**@param { string } note */
     set note(note) {
-        if (typeof note !== 'string') {
-            throw new TypeError('Value must be a string')
-        }
-
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
-
-        const todoObj  = JSON.parse(todoString)
-        todoObj.note = note 
-        localStorage.setItem(this.#id, JSON.stringify(todoObj))
+        this.#handleLocalStorageSet('note', note, AllowedTypes.STRING)
     }
 
-    /** @returns { string } todo list as a string */
+    /**@returns { string | Error }*/
     get list() {
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
-
-        const todoObj = JSON.parse(todoString)
-        if (todoObj.list !== undefined) {
-            return todoObj.list
-        }
-        throw new ReferenceError('Not able to access undefined property')
+        return this.#handleLocalStorageGet('list')
     }
 
     /**@param { string } list */
     set list(list) {
-        if (typeof list !== 'string') {
-            throw new TypeError('Value must be a string')
-        }
-
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
-
-        const todoObj  = JSON.parse(todoString)
-        todoObj.list = list
-        localStorage.setItem(this.#id, JSON.stringify(todoObj))
+        this.#handleLocalStorageSet('list', list, AllowedTypes.STRING)
     }
 
-    /** @returns { string } todo priority as a string */
+    /**@returns { number | Error }*/
     get priority() {
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
-
-        const todoObj = JSON.parse(todoString)
-        if (todoObj.priority !== undefined) {
-            return todoObj.priority
-        }
-        throw new ReferenceError('Not able to access undefined property')
+        return this.#handleLocalStorageGet('priority')
     }
 
     /**@param { Priority } priority */
     set priority(priority) {
-        if (typeof priority !== 'number') {
-            throw new TypeError('Value must be a number')
-        }
+        this.#handleLocalStorageSet('priority', priority, AllowedTypes.NUMBER)
+    }
 
-        const todoString = localStorage.getItem(this.#id)
-        if (todoString === null) {
-            throw new ReferenceError('Not able to access value of null')
-        }
+    /**@returns { boolean | Error }*/
+    get complete() {
+        return this.#handleLocalStorageGet('complete')
+    }
 
-        const todoObj  = JSON.parse(todoString)
-        todoObj.priority = priority.toString()
-        localStorage.setItem(this.#id, JSON.stringify(todoObj))
+    /**@param { boolean } complete */
+    set complete(complete) {
+        this.#handleLocalStorageSet('complete', complete, AllowedTypes.BOOLEAN)
     }
 }
