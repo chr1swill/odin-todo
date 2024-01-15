3c5d41f655996f63babe144ff9cdd7d0e3c56820
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-15 16:46:05 -0500

    refactor - remove whitespace from file

--- src/components/buttons/AddButton.js
+++ src/components/buttons/AddButton.js
@@ -1,14 +1,9 @@
-import { PlusSign } from '../icons/PlusSign'
+import { PlusSign } from '../icons/PlusSign';
 
 /**
- * @param { string } buttonText 
+ * @param { string } buttonText
  * #returns { string }
  * */
 export function AddButton(buttonText) {
-    return `
-        <button 
-        class="text-text bg-primary py-1 px-2">
-            ${PlusSign}${buttonText}
-        </button>
-    `
+  return `<button class="text-text bg-primary py-1 px-2">${PlusSign}${buttonText}</button>`;
 }
