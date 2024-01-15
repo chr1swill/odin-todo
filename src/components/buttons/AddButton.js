b96ddac292a5f9460c675574ab40f4caf4a08207
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-15 16:06:22 -0500

    feat - add some styles to button

--- src/components/buttons/AddButton.js
+++ src/components/buttons/AddButton.js
@@ -6,6 +6,9 @@ import { PlusSign } from '../icons/PlusSign'
  * */
 export function AddButton(buttonText) {
     return `
-        <button>${PlusSign}${buttonText}</button>
+        <button 
+        class="text-text bg-primary py-1 px-2">
+            ${PlusSign}${buttonText}
+        </button>
     `
 }
