a8c4fec782ea9c42b072b59de87fcf343cc859b6
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-16 17:42:26 -0500

    feat - add logic to Todo componenet

--- src/components/sections/Todo.js
+++ src/components/sections/Todo.js
@@ -1,3 +1,28 @@
-export function Todo(todoId, todoTitle, todoNote, todoPriority, todoComplete, todoList) {
-    return (``) 
+import { CheckBox } from "../inputs/CheckBox";
+/**@param { string | null } todoId
+ * @param { string | null } todoTitle
+ * @param { string | null } todoNote
+ * @param { number | null } todoPriority
+ * @param { boolean } todoComplete
+ * @param { string | null } todoList
+ * */
+export function Todo(
+	todoId,
+	todoTitle,
+	todoNote,
+	todoPriority,
+	todoComplete,
+	todoList,
+) {
+	return `<div class="flex flex-row gap-2 justify-center" data-id="${todoId === null ? "" : todoId}">
+            ${CheckBox(todoComplete)}
+            <div>
+                <h3 class="text-text text-xl font-bold">${todoTitle}</h3>
+                <div class="flex flex-col gap-1 justify-center text-text font-medium text-xs">
+                    <p>${todoNote === null ? "" : todoNote}</p>
+                    <p>${todoList === null ? "" : todoList}</p>
+                    <p>${todoPriority === null ? "" : todoPriority}</p>
+                </div>
+            </div>
+        </div>`;
 }
