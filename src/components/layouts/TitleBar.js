f47a0e93c19c113f119db6b02a8757f3ebc3a2c7
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-15 19:12:59 -0500

    feat - add more style to TitleBar

--- src/components/layouts/TitleBar.js
+++ src/components/layouts/TitleBar.js
@@ -5,5 +5,5 @@ import { AddButton } from '../buttons/AddButton'
  * @returns { string }
  * */
 export function TitleBar(title, buttonText) {
-    return `<div class="flex flex-row"><h2 class="text-text text-2xl">${title}</h2>${AddButton(buttonText)}</div><hr>`
+    return `<div class="flex flex-row"><h2 class="text-text font-bold text-2xl">${title}</h2>${AddButton(buttonText)}</div><hr class="border-text border-t-2 my-4">`
 }
