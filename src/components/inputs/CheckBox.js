255fe2dab32cc90d506a6aaea2aea91d4e75af35
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-16 18:28:26 -0500

    refactor - put make checkbox make sense for its usecase

--- src/components/inputs/CheckBox.js
+++ src/components/inputs/CheckBox.js
@@ -1,9 +1,14 @@
-/**@param { boolean } isDisabled - choose state: clickable or non-clickable checkbox*/
-export function CheckBox(isDisabled) {
-  return (
-  `<label class="custom-checkbox-wrapper">
-     <input class="form-checkbox" type="checkbox" ${isDisabled ? 'disabled' : '' } />
-     <span class="custom-checkbox-indicator ${isDisabled ? 'disabled-indicator' : '' }"></span>
+/**@param { boolean } isDisabled - choose state: clickable or non-clickable checkbox
+ * @param { boolean } isChecked - choose state: checked or not checked
+ * */
+export function CheckBox(isDisabled, isChecked = false) {
+	return isDisabled
+		? `<label class="custom-checkbox-wrapper">
+     <input class="form-checkbox" type="checkbox" disabled />
+     <span class="disabled-indicator"></span>
    </label>`
-  )
+		: `<label class="custom-checkbox-wrapper">
+     <input class="form-checkbox" type="checkbox" ${isChecked ? "checked" : ""}/>
+     <span class="custom-checkbox-indicator" ></span>
+   </label>`;
 }
