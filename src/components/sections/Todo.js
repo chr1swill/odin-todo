4a630a6af661b36125a4569c457d17a12fdb7eb3
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-16 18:03:43 -0500

    feat - add more variation to Todo component

--- src/components/sections/Todo.js
+++ src/components/sections/Todo.js
@@ -14,14 +14,30 @@ export function Todo(
 	todoComplete,
 	todoList,
 ) {
+    let priority;
+    switch (todoPriority) {
+        case Priority.HIGH:
+            priority = "!!!";
+            break;
+        case Priority.MEDIUM:
+            priority = "!!";
+            break;
+        case Priority.LOW:
+            priority = "!";
+            break;
+        default:
+            priority = "";
+            break;
+    }
+
 	return `<div class="flex flex-row gap-2 justify-center" data-id="${todoId === null ? "" : todoId}">
             ${CheckBox(todoComplete)}
             <div>
-                <h3 class="text-text text-xl font-bold">${todoTitle}</h3>
+                <h3 class="text-text text- font-bold">${todoTitle}</h3>
                 <div class="flex flex-col gap-1 justify-center text-text font-medium text-xs">
                     <p>${todoNote === null ? "" : todoNote}</p>
-                    <p>${todoList === null ? "" : todoList}</p>
-                    <p>${todoPriority === null ? "" : todoPriority}</p>
+                    <p class="${ todoList === null ? "" : "bg-secondary text-text font-semibold"}">${todoList === null ? "" : todoList}</p>
+                    <p class="${priority === "" ? "" : "text-red-500"}">${priority}</p>
                 </div>
             </div>
         </div>`;
