f882c569f56fa925d353543e83ec08956e27da6d
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-16 10:03:47 -0500

    refactor - make template us less string whitespace - fix spell error

--- src/components/inputs/CheckBox.js
+++ src/components/inputs/CheckBox.js
@@ -1,7 +1,9 @@
-/**@param { boolean } isDisabled - choose state: clickable or non-cliable checkbox*/
+/**@param { boolean } isDisabled - choose state: clickable or non-clickable checkbox*/
 export function CheckBox(isDisabled) {
-  return `<label class="custom-checkbox-wrapper">
-            <input class="form-checkbox" type="checkbox" ${isDisabled ? 'disabled' : '' } />
-            <span class="custom-checkbox-indicator ${isDisabled ? 'disabled-indicator' : '' }"></span>
-          </label>`;
+  return (
+  `<label class="custom-checkbox-wrapper">
+     <input class="form-checkbox" type="checkbox" ${isDisabled ? 'disabled' : '' } />
+     <span class="custom-checkbox-indicator ${isDisabled ? 'disabled-indicator' : '' }"></span>
+   </label>`
+  )
 }
