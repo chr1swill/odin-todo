48a03ba20effa065c5d39347adc61ee0007afab5
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-16 09:24:32 -0500

    feat - make CheckBox component + added styles

---
+++ src/components/inputs/CheckBox.js
@@ -0,0 +1,3 @@
+export function CheckBox() {
+return `<label class="custom-checkbox-wrapper"><input class="form-checkbox" type="checkbox" /><span class="custom-checkbox-indicator"></span></label>`
+}
