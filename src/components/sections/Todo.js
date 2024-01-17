b2f1fb9bc748dd72827bb027b4e17efc754a1549
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-16 19:33:01 -0500

    feat - make Todo component handle more cases - null args, etc.

--- src/components/sections/Todo.js
+++ src/components/sections/Todo.js
@@ -1,44 +1,85 @@
 import { CheckBox } from "../inputs/CheckBox";
+
+/**
+ * Checks if all arguments are null.
+ *
+ * @param {...(string|number|boolean|null)} args - The arguments to check.
+ * @returns { boolean } True if all arguments are null, false otherwise.
+ */
+function checkAllArgumentsNull() {
+	/**@type { boolean } */
+	let tmp = false;
+	if (Array.prototype.every.call(arguments, (arg) => arg === null)) {
+		tmp = true;
+	}
+	return tmp;
+}
+
 /**@param { string | null } todoId
  * @param { string | null } todoTitle
  * @param { string | null } todoNote
  * @param { number | null } todoPriority
- * @param { boolean } todoComplete
+ * @param { boolean | null} todoComplete
  * @param { string | null } todoList
  * */
 export function Todo(
-	todoId,
-	todoTitle,
-	todoNote,
-	todoPriority,
-	todoComplete,
-	todoList,
+	todoId = null,
+	todoTitle = null,
+	todoNote = null,
+	todoPriority = null,
+	todoComplete = null,
+	todoList = null,
 ) {
-    let priority;
-    switch (todoPriority) {
-        case Priority.HIGH:
-            priority = "!!!";
-            break;
-        case Priority.MEDIUM:
-            priority = "!!";
-            break;
-        case Priority.LOW:
-            priority = "!";
-            break;
-        default:
-            priority = "";
-            break;
-    }
+	if (
+		checkAllArgumentsNull(
+			todoId,
+			todoTitle,
+			todoNote,
+			todoPriority,
+			todoComplete,
+			todoList,
+		)
+	) {
+		return `<div class="flex flex-row gap-2 justify-center" data-id="">
+            ${CheckBox(true)}
+            <div>
+                <h3 class="h-5"></h3>
+                <div class="h-4"></div>
+            </div>
+        </div>
+        <hr class="border-text border-t-2 my-4">`;
+	} else {
+		let priority;
+		switch (todoPriority) {
+			case Priority.HIGH:
+				priority = "!!!";
+				break;
+			case Priority.MEDIUM:
+				priority = "!!";
+				break;
+			case Priority.LOW:
+				priority = "!";
+				break;
+			default:
+				priority = "";
+				break;
+		}
 
-	return `<div class="flex flex-row gap-2 justify-center" data-id="${todoId === null ? "" : todoId}">
-            ${CheckBox(todoComplete)}
+		if (todoComplete === null) {
+			todoComplete = false;
+		}
+
+		return `<div class="flex flex-row gap-2 justify-center" data-id="${todoId === null ? "" : todoId}">
+            ${CheckBox(false, todoComplete)}
             <div>
                 <h3 class="text-text text- font-bold">${todoTitle}</h3>
                 <div class="flex flex-col gap-1 justify-center text-text font-medium text-xs">
                     <p>${todoNote === null ? "" : todoNote}</p>
-                    <p class="${ todoList === null ? "" : "bg-secondary text-text font-semibold"}">${todoList === null ? "" : todoList}</p>
+                    <p class="${todoList === null ? "" : "bg-secondary rounded-lg text-text font-semibold"}">${todoList === null ? "" : todoList}</p>
                     <p class="${priority === "" ? "" : "text-red-500"}">${priority}</p>
                 </div>
             </div>
-        </div>`;
+        </div>
+        <hr class="border-text border-t-2 my-4">`;
+	}
 }
