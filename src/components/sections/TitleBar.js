137d604909bc3f713f7a22eb7a298dc523121d8e
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-17 21:23:22 -0500

    feat - add space between title and button

--- src/components/sections/TitleBar.js
+++ src/components/sections/TitleBar.js
@@ -5,5 +5,5 @@ import { AddButton } from '../buttons/AddButton'
  * @returns { string }
  * */
 export function TitleBar(title, buttonText) {
-    return `<div class="flex flex-row"><h2 class="text-text font-bold text-2xl">${title}</h2>${AddButton(buttonText)}</div><hr class="border-text border-t-2 my-4">`
+    return `<div class="flex flex-row content-between"><h2 class="text-text font-bold text-2xl">${title}</h2>${AddButton(buttonText)}</div><hr class="border-text border-t-2 my-4">`
 }
