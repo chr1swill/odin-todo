055292b36fe377d424dca32bf45fc8ce49791488
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-19 15:11:27 -0500

    feat - make arrow component

---
+++ src/components/icons/Arrow.js
@@ -0,0 +1,3 @@
+export function Arrow() {
+	return `<svg width="100" height="50" xmlns="http://www.w3.org/2000/svg"><!-- Arrow Line --><line x1="10" y1="25" x2="90" y2="25" class="stroke-text" stroke-width="2" stroke-linecap="round" /><!-- Arrow Head --><line x1="80" y1="15" x2="90" y2="25" class="stroke-text" stroke-width="2" stroke-linecap="round" /><line x1="80" y1="35" x2="90" y2="25" class="stroke-text" stroke-width="2" stroke-linecap="round" /></svg>`;
+}
