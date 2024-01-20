ed782f67d767d78623c2817ead4519ddec73ccc0
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-19 19:06:49 -0500

    feat - make list modal

---
+++ src/components/modals/CreateListModal.js
@@ -0,0 +1,36 @@
+import { DefaultInputComponent } from "../inputs/DefaultInput";
+import { DefaultButtonComponent } from "../buttons/DefaultButton";
+import { List } from "../../logic/list";
+
+export function ListModalComponet() {
+    const cancelBtn = document.createElement("div");
+    const createBtn = document.createElement("div");
+
+    cancelBtn.innerHTML = DefaultButtonComponent("Cancel");
+    createBtn.innerHTML = DefaultButtonComponent("Create List", "submit", "sumbit");
+
+    cancelBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        cancelBtn?.closest("dialog")?.close();
+    });
+
+    createBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const listName = createBtn?.parentElement?.closest("input")?.value;
+        if (listName) {
+            new List(listName);
+        }
+    })
+
+    return (
+        `<dialog close class="bg-primary rounded-md p-5 max-w-[calc(100vw-2rem)] backdrop:bg-text/50">
+            <form method="dialog">
+                ${DefaultInputComponent("List Name", true)}
+                <div class="grid grid-col-2 gap-3">
+                    ${cancelBtn}
+                    ${createBtn}
+                </div>
+            </form>
+        </dialog>`
+    );
+}
