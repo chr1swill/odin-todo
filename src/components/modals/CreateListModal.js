92df8caa3b0b744788aa2fa5cf1982e6c3509b2a
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-19 22:09:11 -0500

    feat - make logic handle error

--- src/components/modals/CreateListModal.js
+++ src/components/modals/CreateListModal.js
@@ -3,27 +3,37 @@ import { DefaultButtonComponent } from "../buttons/DefaultButton";
 import { List } from "../../logic/list";
 
 export function ListModalComponet() {
-    const cancelBtn = document.createElement("div");
-    const createBtn = document.createElement("div");
+	const cancelBtn = document.createElement("div");
+	const createBtn = document.createElement("div");
 
-    cancelBtn.innerHTML = DefaultButtonComponent("Cancel");
-    createBtn.innerHTML = DefaultButtonComponent("Create List", "submit", "sumbit");
+	cancelBtn.innerHTML = DefaultButtonComponent("Cancel");
+	createBtn.innerHTML = DefaultButtonComponent(
+		"Create List",
+		"submit",
+		"sumbit",
+	);
 
-    cancelBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        cancelBtn?.closest("dialog")?.close();
-    });
+	cancelBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		cancelBtn?.closest("dialog")?.close();
+	});
 
-    createBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        const listName = createBtn?.parentElement?.closest("input")?.value;
-        if (listName) {
-            new List(listName);
-        }
-    })
+	createBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		try {
+			const listName = createBtn?.parentElement?.closest("input")?.value;
+			if (listName) {
+				new List(listName);
+				createBtn?.closest("dialog")?.close();
+			}
+            createBtn?.closest("dialog")?.close();
+			throw new Error("List was not created: Could not access list name");
+		} catch (error) {
+			console.error(error);
+		}
+	});
 
-    return (
-        `<dialog close class="bg-primary rounded-md p-5 max-w-[calc(100vw-2rem)] backdrop:bg-text/50">
+	return `<dialog close class="bg-primary rounded-md p-5 max-w-[calc(100vw-2rem)] backdrop:bg-text/50">
             <form method="dialog">
                 ${DefaultInputComponent("List Name", true)}
                 <div class="grid grid-col-2 gap-3">
@@ -31,6 +41,5 @@ export function ListModalComponet() {
                     ${createBtn}
                 </div>
             </form>
-        </dialog>`
-    );
+        </dialog>`;
 }
