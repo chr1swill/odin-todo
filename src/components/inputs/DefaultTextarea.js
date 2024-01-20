26f1291e246000e6068b809a9554a598fabc3b6b
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-19 22:30:09 -0500

    feat - make textarea component

---
+++ src/components/inputs/DefaultTextarea.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * Create a simple input 
+ * @param { string } placeholder - the text that will appear inside an none active input
+ * @param { boolean } [required=false] - if the input is required
+ * */
+export function DefaultInputComponent(placeholder, required = false) {
+    const name = placeholder.toLowerCase().trim().replace(" ", "-");
+    return (
+        `<label for="${name}" class="hidden">${placeholder}</label>
+        <textarea name="${name}" class="bg-primary py-2 px-3 text-text font-bold text-base border-none rounded-md max-w-[100%] h-auto focus:border-accent" ${required ? "required" : ""}>
+            ${placeholder}
+        </textarea>`
+    )
+}
