b654bc55407120c121182c2d106e41640cfcda4c
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-21 15:47:59 -0500

    refactor - make logic work without htmlElement - will fix to use html elements and not string

--- src/components/modals/CreateTodoModal.js
+++ src/components/modals/CreateTodoModal.js
@@ -2,38 +2,88 @@ import { DefaultInputComponent } from '../inputs/DefaultInput'
 import { DefaultTextareaComponent } from '../inputs/DefaultTextarea'
 import { DefaultDropdownComponent } from '../inputs/DefaultDropdown'
 import { DefaultButtonComponent } from '../buttons/DefaultButton'
+import { Todo, Priority } from '../../logic/todo'
 /**
  *
- *
- * i am broke fix my import and all other componets to return html element and not sting bc they are more flexible and useful
- * you can dom parse after the fact but you loose alot of the control you add before 
- * you can also make them in a more web component way if you want also as you create them in such way that the content you need to change is not in the string 
- * IS REAL HTML ELEMENT
+ * code may work but it need to be refactored to make use without the DOMParser object
+ * going to refactor to make component return real htmlElements
  *
  * */
 export function TodoModalComponent() {
-    const card = document.createElement('div')
-    card.classList.add('grid', 'place-items-center', 'bg-background', 'p-4', 'rounded-md', 'w-[90%]', 'max-w-[400px]') 
+    const parser = new DOMParser();
+    const dialog = document.createElement('dialog');
+    dialog.setAttribute('close', '');
+    dialog.classList.add(
+        'grid', 'place-items-center', 'bg-primary', 'rounded-md', 'p-4', 'max-w-[calc(100vw-2rem)]', 'backdrop:bg-text/50'
+    )
 
-    card.innerHTML = ( 
+    /**@type {HTMLInputElement}*/
+    const input = parser.parseFromString(DefaultInputComponent('Title', true), 'text/html')
+    /**@type {HTMLTextAreaElement}*/
+    const textarea = parser.parseFromString(DefaultTextareaComponent('Notes', true), 'text/html')
+    const dropdown = DefaultDropdownComponent('Choose Priority', ['None','Low', 'Medium', 'High'])
+    /**@type {HTMLButtonElement}*/
+    const cancelBtn = parser.parseFromString(DefaultButtonComponent('Cancel'), 'text/html')
+    /**@type {HTMLButtonElement}*/
+    const createBtn = parser.parseFromString(DefaultButtonComponent('Create Todo', 'submit', 'sumbit'), 'text/html')
+
+	cancelBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+        dialog.close();
+	});
+
+	createBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		try {
+			const todoName = input.value;
+            const todoNote = textarea.value;
+
+            const selectInDropdown = dropdown?.querySelector('select')
+            if (!selectInDropdown) {
+                throw new Error("Could not find select element inside of dropdown")
+            }
+            const todoPriority = selectInDropdown.options[selectInDropdown.selectedIndex]?.textContent
+
+			if (!todoName || !todoNote || !todoPriority) {
+                throw new Error("Todo was not created: Could not access properties of todo");
+			}
+            const todo = new Todo()
+            todo.title = todoName
+            todo.note = todoNote
+
+            switch (todoPriority.toString().trim().toLowerCase()) {
+                case 'none':
+                    todo.priority = Priority.NONE
+                    break;
+                case 'low':
+                    todo.priority = Priority.LOW
+                    break;
+                case 'medium':
+                    todo.priority = Priority.MEDIUM
+                    break;
+                case 'high':
+                    todo.priority = Priority.HIGH
+                    break;
+                default:
+                    throw new Error("Todo was not created: Could provided prirority was invalid");
+            }
+
+		} catch (error) {
+			console.error(error);
+		}
+	});
+
+    return dialog.innerHTML = (
         `
-        ${DefaultInputComponent('Title', true)}
-        ${DefaultTextareaComponent('Notes', true)}
-        ${DefaultDropdownComponent('Choose Priority', ['None','Low', 'Medium', 'High'])}
-        <div class="flex flex-row gap-2">
-            ${DefaultButtonComponent('Cancel')}
-            ${DefaultButtonComponent('Create Todo')}
-        </div>
+        <form method="dialog">
+            ${input}
+            ${textarea}
+            ${dropdown}
+            <div class="flex flex-row gap-2">
+                ${cancelBtn}
+                ${createBtn}
+            </div>
+        </form>
         `
     )
-    const input = DefaultInputComponent('Title', true)
-
-    const p = new DOMParser().parseFromString(input, 'text/html')
-    p.addEventListener('change', (e) => {
-        
-    })
-    const dropDown = DefaultDropdownComponent('Choose Priority', ['None','Low', 'Medium', 'High'])
-    dropDown.addEventListener('change', (e) => {
-        
-    })
 }
