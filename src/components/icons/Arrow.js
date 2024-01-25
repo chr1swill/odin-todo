b82919e149f897dab8b001ad301fdb2a7d10ead3
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-25 14:33:22 -0500

    feat - make arrow look like other icons

--- src/components/icons/Arrow.js
+++ src/components/icons/Arrow.js
@@ -1,5 +1,12 @@
 export function ArrowComponent() {
-	const template = document.createElement("template");
-	template.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"><!-- Arrow Line --><line x1="10" y1="25" x2="90" y2="25" class="stroke-text" stroke-width="2" stroke-linecap="round" /><!-- Arrow Head --><line x1="80" y1="15" x2="90" y2="25" class="stroke-text" stroke-width="2" stroke-linecap="round" /><line x1="80" y1="35" x2="90" y2="25" class="stroke-text" stroke-width="2" stroke-linecap="round" /></svg>`;
-	return template.content.cloneNode(true);
+    const template = document.createElement("template");
+    template.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="20 20 60 60">
+        <!-- Arrow Line -->
+        <line x1="30" y1="50" x2="70" y2="50" class="stroke-text" stroke-width="14" stroke-linecap="round" />
+        <!-- Arrow Head -->
+        <line x1="60" y1="40" x2="70" y2="50" class="stroke-text" stroke-width="14" stroke-linecap="round" />
+        <line x1="60" y1="60" x2="70" y2="50" class="stroke-text" stroke-width="14" stroke-linecap="round" />
+    </svg>`;
+    return template.content.cloneNode(true);
 }
