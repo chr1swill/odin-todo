7178539aec1edeb514ec57e858554c2fa8faaddc
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-25 21:27:22 -0500

    feat - make all element in addButton centered

--- src/components/buttons/AddButton.js
+++ src/components/buttons/AddButton.js
@@ -6,7 +6,7 @@ import { PlusSignComponent } from "../icons/PlusSign";
 export function AddButtonComponent(buttonText) {
 	const button = document.createElement("button");
 	button.className =
-		"flex flex-row place-items-center gap-2 bg-primary hover:bg-secondary transition ease-out duration-1 p-1 text-text font-bold text-base rounded";
+		"flex flex-row place-content-center place-items-center gap-2 bg-primary hover:bg-secondary transition ease-out duration-1 p-1 text-text font-bold text-base rounded";
 
 	const iconWrapper = document.createElement("span");
 	iconWrapper.className = "";
