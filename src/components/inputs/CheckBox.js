f3457bbe1e01b29c5eb38078853624475200afea
chr1swill <alexanderwilliams20003@gmail.com>
2024-02-02 19:03:26 -0500

    refactor: remove unused logic

--- src/components/inputs/CheckBox.js
+++ src/components/inputs/CheckBox.js
@@ -1,38 +1,3 @@
-/**
- * TODO:
- * Setup the this function to do something like:
- * change state in localStorage,
- * if complete remove from storage,
- * then reRender list of todo
- *
- * this show happen in time out to change sure it was not just checked accidentally
- */
-function handleInputStateChange() {
-	console.log(
-		"CheckBox input state changed TODO: set upt logic to handle input being checked",
-	);
-}
-
-/**
- * @param {HTMLInputElement} input
- */
-function setupInput(input) {
-	const handleStateChange = () => handleInputStateChange();
-	input?.addEventListener("change", handleStateChange);
-
-	const observer = new MutationObserver((mutation) => {
-		let i = 0;
-		while (i < mutation.length) {
-			if (!document.body.contains(input)) {
-				input.removeEventListener("change", handleStateChange);
-				observer.disconnect();
-				break;
-			}
-			i++;
-		}
-	});
-	observer.observe(document.body, { childList: true, subtree: true });
-}
 
 /**
  * @param { boolean } [isDisabled=false] - choose state: clickable or non-clickable checkbox
@@ -52,12 +17,9 @@ export function CheckBoxComponent(isDisabled = false, isChecked = false) {
 	if (isDisabled) {
 		input.setAttribute("disabled", "");
 	}
-
 	if (isChecked) {
 		input.setAttribute("checked", "");
 	}
-
-	setupInput(input);
 
 	label.append(input, span);
 
